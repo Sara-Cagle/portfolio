ce2585d490d6f036a6263769c79cdce1a0998d3e
Sara Cagle <cagle.sara@gmail.com>
2015-09-21 22:53:24 +0800

    Begin controller desigign

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,22 @@
-angular.module('PortfolioApp.controllers', []).
+/*angular.module('PortfolioApp.controllers', []).
 controller('portfolioController', function($scope) {
     //$scope.driversList = [];
-});
+});*/
 
+
+var portfolioController = function($scope){
+};
+
+var navController = function($scope){
+	$scope.links =[
+		{linkName: 'About',
+		URL: '',
+		},
+		{linkName: 'Resume',
+		URL: '',
+		},
+		{linkName: 'Projects',
+		URL: '',
+		}
+	]
+};
