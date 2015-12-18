03307a4e2a9d532708a42e9d98a9c89ce1bec9a2
Sara Cagle <cagle.sara@gmail.com>
2015-12-18 01:30:00 -0500

    page anchors

--- site/js/controllers.js
+++ site/js/controllers.js
@@ -18,6 +18,15 @@ angular.module('PortfolioApp', ['ngRoute', 'ngAnimate'])
 	]
 }])
 
+.controller('contentController', [function($route, $routeParams) {
+	var links = document.getElementsByClassName("nav-link");
+	for (var i = 0; i < links.length; ++i) {
+		links[i].onclick = function(){
+   			document.getElementById('slide').scrollIntoView();
+  		}
+ 	}
+}])
+
 .config(function($routeProvider) { //routing needs to be on a server in order to run
 	$routeProvider
 	.when('/',{
