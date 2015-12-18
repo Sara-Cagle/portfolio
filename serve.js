51e5169e99c50014f823a5ff6e7bbf2e9bd1a227
EVAN S. COMPTON <escompto@us.ibm.com>
2015-12-18 15:30:42 -0600

    fixed the things

--- serve.js
+++ serve.js
@@ -1,6 +1,24 @@
-var express = require('express');
-var app = express();
+"use strict"
+let express = require('express');
+let app = express();
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let logger = require('morgan');
+let http = require("http");
 
-app.use('/', express.static(__dirname + '/site')) ;
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.listen(8080);
+app.use(express.static(path.join(__dirname,'/site/public')))
+
+app.use('/', function(req, res, next) {
+	res.sendFile(path.join(__dirname,'/site/index.html'))
+}) ;
+
+app.set("port", 3000);
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
